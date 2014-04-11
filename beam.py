35a5b4779d17b27ab1e93c28cd2f3b30bf735f4b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-11 16:50:56 -0400

    minor formatting fixes

--- beam.py
+++ beam.py
@@ -1,101 +1,82 @@
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-# IMPORTS
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
 # Astropy required or not?
+from astropy import units as u
 from astropy.io import fits
 from math import sqrt, pi, cos, sin, abs, atan2
 import numpy as np
 
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-# OBJECT
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
 class Beam(object):
-    """ 
+    """
     An object to handle radio beams.
     """
 
-    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Attributes
-    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=    
-
     major = None
     minor = None
-    pa = None    
+    pa = None
 
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Constructor
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    def __init__(
-        self,
-        major=None,
-        minor=None,
-        pa = None,
-        area = None,
-        fname = None,
-        hdr = None,
-        ):
+    def __init__(self, major=None, minor=None, pa=None, area=None, fname=None,
+                 hdr=None,):
         """
+
+        Parameters
+        ----------
         """
         
         # improve to some kwargs magic later
 
         # error checking
 
-        # ... given an area make a round beam        
-        if area != None:
+        # ... given an area make a round beam
+        if area is not None:
             rad = sqrt(area/pi)
             self.major = rad
             self.minor = rad
             self.pa = 0.0
-            return
             
         # ... given a file try to make a fits header
-        if fname != None:
+        if fname is not None:
             hdr = fits.getheader(fname)
 
-        if hdr != None:
-            if hdr.has_key("BMAJ"):
+        if hdr is not None:
+            if "BMAJ" in hdr:
                 self.major = hdr["BMAJ"]
             else:
-                if self.from_aips_header(hdr) == False:
+                if not self.from_aips_header(hdr):
                     print "No keyword BMAJ or AIPS convention."
                     # ... exit with blank object
                     return
 
-            if hdr.has_key("BMIN"):
+            if "BMIN" in hdr:
                 self.minor = hdr["BMIN"]
-            if hdr.has_key("BPA"):
+            if "BPA" in hdr:
                 self.pa = hdr["BPA"]
             # ... logic for case of no keyword
             # ... get AIPS?
                 
         # give specified values priority
-        if major != None:
+        if major is not None:
             self.major = major
-        if minor != None:
+        if minor is not None:
             self.minor = minor
-        if pa != None:
+        if pa is not None:
             self.pa = pa
 
         # some sensible defaults
-        if self.minor == None:
+        if self.minor is None:
             self.minor = self.major
 
-        if self.pa == None:
+        if self.pa is None:
             self.pa = 0.0
-
-        pass
 
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Functions to get the beam
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    def from_aips_header(
-        self,
-        hdr):
+    def from_aips_header(self, hdr):
         """
         Extract the beam from an old AIPS header. Returns true if
         successful?
@@ -109,15 +90,13 @@ class Beam(object):
         # multiple cleans can be in there, so you need to work
         # BACKWARDS to get the last line that has this.
 
-        return False
+        raise NotImplementedError()
 
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Operators
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    def __add__(
-        self,
-        other):
+    def __add__(self, other):
         """
         Addition convolves.
         """
@@ -126,23 +105,23 @@ class Beam(object):
 
         # blame: https://github.com/pkgw/carma-miriad/blob/CVSHEAD/src/subs/gaupar.for
 
-        alpha = (self.major*cos(self.pa))**2 + \
-            (self.minor*sin(self.pa))**2 + \
-            (other.major*cos(other.pa))**2 + \
-            (other.minor*sin(other.pa))**2
+        alpha = ((self.major*cos(self.pa))**2 +
+                 (self.minor*sin(self.pa))**2 +
+                 (other.major*cos(other.pa))**2 +
+                 (other.minor*sin(other.pa))**2)
 
-        beta = (self.major*sin(self.pa))**2 + \
-            (self.minor*cos(self.pa))**2 + \
-            (other.major*sin(other.pa))**2 + \
-            (other.minor*cos(other.pa))**2
-	
-	gamma = 2*((self.minor**2-self.major**2)* \
-                       sin(self.pa)*cos(self.pa) + \
-                       (other.minor**2-other.major**2)* \
-                       sin(other.pa)*cos(other.pa))
+        beta = ((self.major*sin(self.pa))**2 +
+                (self.minor*cos(self.pa))**2 +
+                (other.major*sin(other.pa))**2 +
+                (other.minor*cos(other.pa))**2)
+        
+        gamma = (2*((self.minor**2-self.major**2) *
+                    sin(self.pa)*cos(self.pa) +
+                    (other.minor**2-other.major**2) *
+                    sin(other.pa)*cos(other.pa)))
 
         s = alpha + beta
-	t = sqrt((alpha-beta)**2 + gamma**2)
+        t = sqrt((alpha-beta)**2 + gamma**2)
 
         new_major = sqrt(0.5*(s+t))
         new_minor = sqrt(0.5*(s-t))
@@ -151,7 +130,6 @@ class Beam(object):
         else:
             new_pa = 0.5*atan2(-1.*gamma, alpha-beta)
             # units!
-
         
         # Make a new beam and return it
         return Beam(major=new_major,
@@ -160,20 +138,16 @@ class Beam(object):
 
     # Does multiplication do the same? Or what?
     
-    def __sub__(
-        self,
-        other):
+    def __sub__(self, other):
         """
-        Addition deconvolves.
+        Subtraction deconvolves.
         """
         # math.
         pass
 
     # Does division do the same? Or what? Doesn't have to be defined.
 
-    def __eq__(
-        self,
-        other):
+    def __eq__(self, other):
         """
         Equality operator.
         """
