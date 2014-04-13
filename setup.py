337661ed5f62fc3d598cf645951412ad0d183323
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-13 15:37:30 -0400

    setup fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if 'build_sphinx' in sys.argv or 'develop' in sys.argv:
 else:
     from distutils.core import setup, Command
 
-with open('README.rst') as file:
+with open('README.md') as file:
     long_description = file.read()
 
 with open('CHANGES') as file:
