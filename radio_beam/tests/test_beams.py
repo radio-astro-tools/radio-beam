e458b1fb5ecedc06a8a1c839ba804a6c9633093b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-24 15:41:55 -0600

    add test

--- radio_beam/tests/test_beams.py
+++ radio_beam/tests/test_beams.py
@@ -1,7 +1,10 @@
+import numpy as np
 
+from astropy import units as u
 from astropy.io import fits
 
 from ..multiple_beams import Beams
+from ..beam import Beam
 
 from .test_beam import data_path
 
@@ -17,3 +20,14 @@ def test_beams_from_fits_bintable():
     assert (beams.majors.value == bintable.data['BMAJ']).all()
     assert (beams.minors.value == bintable.data['BMIN']).all()
     assert (beams.pas.value == bintable.data['BPA']).all()
+
+def test_indexing():
+
+    beams = Beams(majors=[1,1,1,2,3,4]*u.arcsec)
+
+    assert np.all(beams[:3].major.value == [1,1,1])
+
+    assert beams[3].major.value == 2
+    assert isinstance(beams[4], Beam)
+
+    assert np.all(beams[np.array([True,False,True,False,True,True], dtype='bool')].major.value == [1,1,3,4])
