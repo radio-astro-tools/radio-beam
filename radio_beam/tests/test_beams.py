3ef171d5fa982fbc744ff6bcfa46f5b0942cca56
e-koch <koch.eric.w@gmail.com>
2017-06-03 18:01:56 -0600

    Add tests for new beam operations; add in checks for the minor axis

--- radio_beam/tests/test_beams.py
+++ radio_beam/tests/test_beams.py
@@ -9,6 +9,13 @@ from ..beam import Beam
 from .test_beam import data_path
 
 
+def beams_for_tests():
+
+    majors = [1, 1, 1, 2, 3, 4] * u.arcsec
+
+    return Beams(major=majors), majors
+
+
 def test_beams_from_fits_bintable():
 
     fname = data_path("m33_beams_bintable.fits.gz")
@@ -21,19 +28,59 @@ def test_beams_from_fits_bintable():
     assert (beams.minor.value == bintable.data['BMIN']).all()
     assert (beams.pa.value == bintable.data['BPA']).all()
 
+
 def test_indexing():
 
-    beams = Beams(major=[1,1,1,2,3,4]*u.arcsec)
+    beams, majors = beams_for_tests()
 
-    assert hasattr(beams[slice(0,3)], 'major')
-    assert np.all(beams[slice(0,3)].major.value == [1,1,1])
+    assert hasattr(beams[slice(0, 3)], 'major')
+    assert np.all(beams[slice(0, 3)].major.value == majors[:3].value)
+    assert np.all(beams[slice(0, 3)].minor.value == majors[:3].value)
 
     assert hasattr(beams[:3], 'major')
-    assert np.all(beams[:3].major.value == [1,1,1])
+    assert np.all(beams[:3].major.value == majors[:3].value)
+    assert np.all(beams[:3].minor.value == majors[:3].value)
 
     assert hasattr(beams[3], 'major')
     assert beams[3].major.value == 2
+    assert beams[3].minor.value == 2
     assert isinstance(beams[4], Beam)
 
-    assert hasattr(beams[np.array([True,False,True,False,True,True], dtype='bool')], 'major')
-    assert np.all(beams[np.array([True,False,True,False,True,True], dtype='bool')].major.value == [1,1,3,4])
+    mask = np.array([True, False, True, False, True, True], dtype='bool')
+    assert hasattr(beams[mask], 'major')
+    assert np.all(beams[mask].major.value == majors[mask].value)
+
+
+def test_average_beam():
+
+    beams, majors = beams_for_tests()
+
+    assert np.all(beams.average_beams().major.value == majors.mean().value)
+
+
+def test_largest_beam():
+
+    beams, majors = beams_for_tests()
+
+    assert np.all(beams.largest_beam().major.value == majors.max().value)
+    assert np.all(beams.largest_beam().minor.value == majors.max().value)
+
+
+def test_smallest_beam():
+
+    beams, majors = beams_for_tests()
+
+    assert np.all(beams.smallest_beam().major.value == majors.min().value)
+    assert np.all(beams.smallest_beam().minor.value == majors.min().value)
+
+
+def test_extrema_beam():
+
+    beams, majors = beams_for_tests()
+
+    extrema = beams.extrema_beams()
+    assert np.all(extrema[0].major.value == majors.min().value)
+    assert np.all(extrema[0].minor.value == majors.min().value)
+
+    assert np.all(extrema[1].major.value == majors.max().value)
+    assert np.all(extrema[1].minor.value == majors.max().value)
