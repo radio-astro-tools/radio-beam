a0da776b02f03e5172bad1deda28ffc059aa4253
e-koch <koch.eric.w@gmail.com>
2017-06-05 14:21:38 -0600

    Same for convolve

--- radio_beam/utils.py
+++ radio_beam/utils.py
@@ -135,7 +135,7 @@ def convolve(beam, other):
 
     new_major = np.sqrt(0.5 * (s + t))
     new_minor = np.sqrt(0.5 * (s - t))
-    if (abs(gamma) + abs(alpha - beta)) == 0:
+    if np.isclose((abs(gamma) + abs(alpha - beta)).value, 0):
         new_pa = 0.0 * u.deg
     else:
         new_pa = 0.5 * np.arctan2(-1. * gamma, alpha - beta)
