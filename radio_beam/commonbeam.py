6fb2e2f3eba04a77ec170c38a61cd5f191f96035
e-koch <koch.eric.w@gmail.com>
2017-06-05 23:00:58 -0600

    Redefine largest major and minor in the loop; commented out debugging statements

--- radio_beam/commonbeam.py
+++ radio_beam/commonbeam.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import astropy.units as u
+from astropy import log
 
 from .beam import Beam
 from .utils import BeamError, transform_ellipse
@@ -28,24 +29,28 @@ def commonbeam(beams):
         raise BeamError("All beams in the object are invalid.")
 
     largest_beam = beams.largest_beam()
-    largest_major = largest_beam.major
-    largest_minor = largest_beam.minor
+    largest_major = largest_beam.major.to(u.arcsec)
+    largest_minor = largest_beam.minor.to(u.arcsec)
 
     for beam in beams:
-
         if beam != largest_beam and beam.isfinite:
             deconv_beam = \
                 largest_beam.deconvolve(beam, failure_returns_pointlike=True)
             if not deconv_beam.isfinite:
+                # log.info("Cannot deconvolve: {0}, {1}, {2}".format(beam.major, beam.minor, beam.pa))
+                # log.info("Largest beam: {0}, {1}, {2}".format(largest_beam.major, largest_beam.minor, largest_beam.pa))
+                pa_diff = beam.pa.to(u.rad) - largest_beam.pa.to(u.rad)
 
-                pa_diff = (beam.pa - largest_beam.pa).to(u.rad)
+                largest_major = largest_beam.major.to(u.arcsec)
+                largest_minor = largest_beam.minor.to(u.arcsec)
 
-                problem_major = beam.major
-                problem_minor = beam.minor
+                problem_major = beam.major.to(u.arcsec)
+                problem_minor = beam.minor.to(u.arcsec)
 
                 # If the difference is pi / 2, the larger major is set to the
                 # new major and the minor is the other major.
-                if np.abs(pa_diff).value == np.pi / 2.:
+                if np.isclose(np.abs(pa_diff).value, np.pi / 2.):
+                    # log.info("PA differs by 90 deg. Return largest in each direction.")
                     larger_major = largest_beam.major >= beam.major
                     major = largest_major if larger_major else problem_major
                     minor = problem_major if larger_major else largest_major
@@ -78,18 +83,26 @@ def commonbeam(beams):
 
                 # Lastly, rotate the PA to the enclosing ellipse
 
-                trans_major = trans_major_unsc
-                trans_minor = trans_minor_unsc
+                trans_major = trans_major_unsc.to(u.arcsec)
+                trans_minor = trans_minor_unsc.to(u.arcsec)
                 trans_pa = trans_pa_unsc + largest_beam.pa
 
                 trans_beam = Beam(major=trans_major, minor=trans_minor,
                                   pa=trans_pa)
+
+                # log.info("Found transformed common beam with: {0}, {1}, {2}".format(trans_beam.major, trans_beam.minor, trans_beam.pa))
 
                 # Ensure this beam can now be deconvolved
                 # Scale the enclosing ellipse by a small factor until it does.
                 can_deconv = False
                 num = 0
                 while not can_deconv:
+
+                    # print(num)
+
+                    # if num == 2:
+                    #     print(argh)
+
                     deconv_large_beam = \
                         trans_beam.deconvolve(largest_beam,
                                               failure_returns_pointlike=True)
@@ -100,13 +113,14 @@ def commonbeam(beams):
                     if (not deconv_large_beam.isfinite or
                         not deconv_prob_beam.isfinite):
 
-                        scale_factor = 1.0 + 1e-8
+                        scale_factor = 1.001
                         trans_beam = Beam(major=trans_major * scale_factor,
                                           minor=trans_minor * scale_factor,
                                           pa=trans_pa)
-                        num += 1
                     else:
                         can_deconv = True
+
+                    num += 1
 
                 largest_beam = trans_beam
 
