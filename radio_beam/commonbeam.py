8049dfd4553eb41f59104ab02d98aad39110a5ad
e-koch <koch.eric.w@gmail.com>
2018-01-01 10:43:52 -0800

    Set boundary cases to 0.

--- radio_beam/commonbeam.py
+++ radio_beam/commonbeam.py
@@ -461,7 +461,7 @@ def ellipse_edges(beam, npts=300, epsilon=1e-3):
     return pts
 
 
-def common_manybeams_mve(beams, tolerance=1e-4, nsamps=200, epsilon=1e-3):
+def common_manybeams_mve(beams, tolerance=1e-4, nsamps=200, epsilon=5e-4):
     '''
     Calculate a common beam size using the Khachiyan Algorithm to find the
     minimum enclosing ellipse from all beam edges.
@@ -505,8 +505,13 @@ def common_manybeams_mve(beams, tolerance=1e-4, nsamps=200, epsilon=1e-3):
     # The rotation matrix is coming out as:
     # ((sin theta, cos theta)
     #  (cos theta, - sin theta))
+    pa = np.arctan2(- rotation[0, 0], rotation[1, 0]) * u.rad
+
+    if pa.value == -np.pi or pa.value == np.pi:
+        pa = 0.0 * u.rad
+
     com_beam = Beam(major=radii.max() * u.deg, minor=radii.min() * u.deg,
-                    pa=np.arctan2(- rotation[0, 0], rotation[1, 0]) * u.rad)
+                    pa=pa)
 
     if not fits_in_largest(beams, com_beam):
         raise BeamError("Could not find common beam to deconvolve all beams.")
